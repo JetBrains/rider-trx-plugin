ed0ae4b1b973081eecf2d2994c42fc14cc3395e9
Nikita <nikiniks2005@gmail.com>
2024-08-03 16:53:46 +0300

    Delete IRiderPluginsTemplateZone.cs

--- src/dotnet/Rider.Plugins.TrxPlugin/IRiderPluginsTemplateZone.cs
+++
@@ -1,7 +0,0 @@
-using JetBrains.Application.BuildScript.Application.Zones;
-using JetBrains.ProjectModel;
-
-namespace Rider.Plugins.TrxPlugin;
-
-[ZoneMarker]
-public class ZoneMarker : IRequire<IProjectModelZone>;
