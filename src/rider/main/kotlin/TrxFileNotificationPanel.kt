144e84b5934604530928e15fd8d138042e5389ba
Artem Abaturov <artemabaturov75@gmail.com>
2024-08-08 13:22:35 +0300

    Add info status for panel

--- src/rider/main/kotlin/TrxFileNotificationPanel.kt
+++ src/rider/main/kotlin/TrxFileNotificationPanel.kt
@@ -4,6 +4,7 @@ import com.intellij.ui.EditorNotificationPanel
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.wm.impl.status.InfoAndProgressPanel
 import com.jetbrains.rider.plugins.trxplugin.ProtocolCaller
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -11,12 +12,11 @@ import kotlinx.coroutines.launch
 import java.awt.Color
 import kotlin.coroutines.CoroutineContext
 
-class TrxFileNotificationPanel(project: Project, file: VirtualFile) : EditorNotificationPanel(), CoroutineScope {
+class TrxFileNotificationPanel(project: Project, file: VirtualFile) : EditorNotificationPanel(Status.Info), CoroutineScope {
     override val coroutineContext: CoroutineContext
         get() = Dispatchers.Main
     init {
         text = "Import this TRX as unit test session"
-        // background = Color(0, 100, 0)
         createActionLabel("Import") {
             launch {
                 val protocolCaller = project.getService(ProtocolCaller::class.java)
