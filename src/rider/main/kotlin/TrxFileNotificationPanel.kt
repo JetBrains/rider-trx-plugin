9269de4212a4cb92749dc882722363aa1320da7d
Nikita <nikiniks2005@gmail.com>
2024-08-09 15:36:45 +0300

    Add an action for trx files in solution explorer

--- src/rider/main/kotlin/TrxFileNotificationPanel.kt
+++ src/rider/main/kotlin/TrxFileNotificationPanel.kt
@@ -1,15 +1,10 @@
-import com.intellij.openapi.editor.colors.ColorKey
-import com.intellij.openapi.editor.colors.EditorColors
 import com.intellij.ui.EditorNotificationPanel
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.openapi.wm.impl.status.InfoAndProgressPanel
 import com.jetbrains.rider.plugins.trxplugin.ProtocolCaller
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import java.awt.Color
 import kotlin.coroutines.CoroutineContext
 
 class TrxFileNotificationPanel(project: Project, file: VirtualFile) : EditorNotificationPanel(Status.Info), CoroutineScope {
---
+++ src/rider/main/kotlin/TrxFileProjectViewAction.kt
@@ -0,0 +1,34 @@
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.jetbrains.rider.plugins.trxplugin.ProtocolCaller
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
+
+
+class TrxFileProjectViewAction : AnAction("Import this TRX as unit test session"), CoroutineScope {
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main
+
+    override fun actionPerformed(event: AnActionEvent) {
+        val file = event.getData(CommonDataKeys.VIRTUAL_FILE)
+        val project = event.project
+        launch {
+            val protocolCaller = project?.getService(ProtocolCaller::class.java)
+            val response = file?.let { protocolCaller?.doCall(it.path) }
+        }
+    }
+
+    override fun update(e: AnActionEvent) {
+        val file = e.getData(CommonDataKeys.VIRTUAL_FILE)
+        e.presentation.isEnabledAndVisible = file != null && file.extension.equals("trx", ignoreCase = true)
+    }
+
+    override fun getActionUpdateThread(): ActionUpdateThread {
+        return ActionUpdateThread.EDT
+    }
+}
