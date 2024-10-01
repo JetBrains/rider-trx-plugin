1e8e7bad9a6a83b7cc57e58d8bb3ee7a612b94cc
Ivan Migalev <ivan.migalev@jetbrains.com>
2024-10-01 20:00:00 +0200

    Backend: fix zone validation errors

---
+++ src/dotnet/Rider.Plugins.TrxPlugin/ZoneMarker.cs
@@ -0,0 +1,7 @@
+﻿using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.ReSharper.UnitTestFramework;
+
+namespace Rider.Plugins.TrxPlugin;
+
+[ZoneMarker]
+public class ZoneMarker : IRequire<IUnitTestingZone>;
